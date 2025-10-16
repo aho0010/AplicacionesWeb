697a67d1354c8dd9aacbd2df5109c556447819b4
aho0010 <aho0010@alu.medac.es>
2025-10-16 20:38:18 +0200

    nuevo

---
+++ TEMA04/EjemploJS/script.js
@@ -0,0 +1,12 @@
+       
+    //document.write("Hola Mundo");
+        var parrafo = document.getElementById("texto");
+        parrafo.innerHTML = "Texto desde JavaScript";
+        parrafo.style.color = "red";
+
+        var contenedor = document.getElementById("contenedor");
+        var nuevo = document.createElement("p");
+        var texto = document.createTextNode("texto desde JavaScript");
+      
+        contenedor.appendChild(nuevo);
+        nuevo.appendChild(texto);
