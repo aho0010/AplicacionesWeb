de731c5ec607e66bcb1a47304de868541dec90c3
aho0010 <aho0010@alu.medac.es>
2025-10-27 20:07:00 +0100

    reee

--- EJERCICIO 2php/proceso.php
+++ EJERCICIO 2php/proceso.php
@@ -1,12 +1,14 @@
 <?php
-$nombre = ($_GET['nombre']);
-$apellidos = ($_GET['apellidos']);
-$edad = intval ($_GET['edad']);
-function comprobarEdad($edad) {
+$nombre = ($_GET["nombre"]);
+$apellidos = ($_GET["apellidos"]);
+$edad = intval ($_GET["edad"]);
+function comprobarEdad($nombre,$apellidos,$edad) {
     if ($edad >= 18) {
         echo "hola $nombre $apellidos, eres mayor de edad.";
     } else {
         echo "hola $nombre $apellidos, eres menor de edad.";
     }
-    comprobarEdad($nombre,$apellidos,$edad);
-}
+   }
+   
+   comprobarEdad($nombre,$apellidos,$edad)
+?>
